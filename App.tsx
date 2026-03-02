74ae39669fb79f6db7eb787dc4f2c824b5d43b63
jasminepro05 <jasmine.khanna315@gmail.com>
2026-03-02 15:27:47 +0530

    initialize main App layou
    React imports
    Component imports
    Basic App component
    currentTab state
    basic return layout

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import React from "react";
-import HomePage from "./components/HomePage";   // added in Commit 3
+import React, { useState } from 'react';
+import { Navbar } from './components/Navbar';
+import { Sidebar } from './components/Sidebar';
+import { LandingPage } from './pages/LandingPage';
 
 const App: React.FC = () => {
+  const [currentTab, setCurrentTab] = useState('home');
+
   return (
-    <>
-      <HomePage />   {/* Commit 3 integration */}
-    </>
+    <div>
+      <Navbar currentTab={currentTab} onTabChange={setCurrentTab} />
+      <Sidebar currentTab={currentTab} onTabChange={setCurrentTab} />
+      <LandingPage />
+    </div>
   );
 };
 
