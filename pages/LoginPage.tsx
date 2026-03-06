8f6143036306833949342da1872c8e5671dacf85
Adhya08 <adhya0208@outlook.com>
2026-03-06 21:42:53 +0530

    added login page and imported files

---
+++ pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { db, User } from '../data/database';
+import { Logo } from '../components/Logo';
+import {
+  ShieldCheck,
+  User as UserIcon,
+  Mail,
+  Lock,
+  Eye,
+  EyeOff,
+  AlertCircle,
+  ArrowRight,
+  Loader2,
+  Info
+} from 'lucide-react';
