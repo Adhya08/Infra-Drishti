59f0318acdb9477b15d78ac2af230a5c4a9f4a83
Adhya08 <adhya0208@outlook.com>
2026-03-09 23:35:06 +0530

    added analysisPage and imported the filesss

---
+++ src/pages/analysisPsge.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Activity,
+  Waves,
+  Truck,
+  Music,
+  Mic,
+  Zap,
+  BarChart3,
+  Bell,
+  AlertTriangle,
+  PieChart,
+  LucideIcon
+} from 'lucide-react';
